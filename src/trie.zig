b9445d87f01ebb3399da58f85b23ba4516e1d948
Ben Grant <benjamin.j.grant@gmail.com>
2022-03-16 01:15:44 -0700

    Make trie.zig entirely a struct

--- src/trie.zig
+++ src/trie.zig
@@ -6,89 +6,89 @@ const ALPHABET = 256;
 
 const Code = @import("./code.zig");
 
-const TrieNode = struct {
-    children: [ALPHABET]?*TrieNode,
-    code: Code.Code,
-    allocator: Allocator,
+const Self = @This();
 
-    fn init(allocator: Allocator, code: Code.Code) !*TrieNode {
-        var n = try allocator.create(TrieNode);
-        n.* = .{
-            .children = [_]?*TrieNode{null} ** ALPHABET,
-            .code = code,
-            .allocator = allocator,
-        };
-        return n;
+children: [ALPHABET]?*Self,
+code: Code.Code,
+allocator: Allocator,
+
+pub fn init(allocator: Allocator, code: Code.Code) !*Self {
+    var n = try allocator.create(Self);
+    n.* = .{
+        .children = [_]?*Self{null} ** ALPHABET,
+        .code = code,
+        .allocator = allocator,
+    };
+    return n;
+}
+
+pub fn create(allocator: Allocator) !*Self {
+    return init(allocator, Code.empty);
+}
+
+pub fn deinit(self: *Self) void {
+    self.allocator.destroy(self);
+}
+
+pub fn reset(self: *Self) void {
+    for (self.children) |c, i| {
+        if (c) |node| {
+            node.delete();
+            self.children[i] = null;
+        }
     }
+}
 
-    fn create(allocator: Allocator) !*TrieNode {
-        return init(allocator, Code.empty);
-    }
-
-    fn deinit(self: *TrieNode) void {
-        self.allocator.destroy(self);
-    }
-
-    fn reset(self: *TrieNode) void {
-        for (self.children) |c, i| {
-            if (c) |node| {
-                node.delete();
-                self.children[i] = null;
-            }
+pub fn delete(self: *Self) void {
+    for (self.children) |c, i| {
+        if (c) |node| {
+            node.delete();
+            self.children[i] = null;
         }
     }
 
-    fn delete(self: *TrieNode) void {
-        for (self.children) |c, i| {
-            if (c) |node| {
-                node.delete();
-                self.children[i] = null;
-            }
-        }
+    self.deinit();
+}
 
-        self.deinit();
-    }
-
-    fn step(self: *TrieNode, symbol: u8) ?*TrieNode {
-        return self.children[symbol];
-    }
-};
+pub fn step(self: *const Self, symbol: u8) ?*Self {
+    return self.children[symbol];
+}
 
 test "TrieNode.init" {
-    const n = try TrieNode.init(std.testing.allocator, 56);
+    const n = try Self.init(std.testing.allocator, 56);
     defer n.deinit();
     try expectEqual(@as(u16, 56), n.code);
     for (n.children) |c| {
-        try expectEqual(@as(?*TrieNode, null), c);
+        try expectEqual(@as(?*Self, null), c);
     }
 }
 
 test "TrieNode.create" {
-    const n = try TrieNode.create(std.testing.allocator);
+    const n = try Self.create(std.testing.allocator);
     defer n.deinit();
     try expectEqual(Code.empty, n.code);
 }
 
 test "TrieNode.reset" {
-    const root = try TrieNode.create(std.testing.allocator);
+    const root = try Self.create(std.testing.allocator);
     defer root.deinit();
-    root.children['a'] = try TrieNode.init(std.testing.allocator, 'a');
+    root.children['a'] = try Self.init(std.testing.allocator, 'a');
     root.reset();
-    try expectEqual(@as(?*TrieNode, null), root.children['a']);
+    try expectEqual(@as(?*Self, null), root.children['a']);
 }
 
 test "TrieNode.delete" {
-    const root = try TrieNode.create(std.testing.allocator);
-    root.children['a'] = try TrieNode.init(std.testing.allocator, 'a');
-    root.children['b'] = try TrieNode.init(std.testing.allocator, 'b');
+    const root = try Self.create(std.testing.allocator);
+    root.children['a'] = try Self.init(std.testing.allocator, 'a');
+    root.children['b'] = try Self.init(std.testing.allocator, 'b');
     root.delete();
 }
 
 test "TrieNode.step" {
-    const root = try TrieNode.create(std.testing.allocator);
+    const root = try Self.create(std.testing.allocator);
     defer root.delete();
-    root.children['a'] = try TrieNode.init(std.testing.allocator, 'a');
-    root.children['b'] = try TrieNode.init(std.testing.allocator, 'b');
+    root.children['a'] = try Self.init(std.testing.allocator, 'a');
+    root.children['b'] = try Self.init(std.testing.allocator, 'b');
     try expectEqual(root.children['a'], root.step('a'));
     try expectEqual(root.children['b'], root.step('b'));
 }
