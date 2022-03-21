154df405e9151ce1f95591eb58e0af1e95685272
Ben Grant <benjamin.j.grant@gmail.com>
2022-03-21 12:11:12 -0700

    Fix and test Word

--- src/Word.zig
+++ src/Word.zig
@@ -3,40 +3,63 @@ const Allocator = std.mem.Allocator;
 
 const Code = @import("./code.zig");
 
-const Self = @This();
+const Word = @This();
 
-ptr: [*]u8,
-len: usize,
+syms: []u8,
 allocator: Allocator,
 
-pub const WordTable = [Code.max]?*Self;
-
-pub fn init(allocator: Allocator, syms: []const u8) !*Self {
-    const w = try allocator.create(Self);
-    errdefer allocator.destroy(w);
-    w.* = .{
-        .ptr = try allocator.alloc(u8, syms.len),
-        .len = syms.len,
+pub fn init(self: *Word, allocator: Allocator, syms: []const u8) !void {
+    self.* = .{
+        .syms = try allocator.alloc(u8, syms.len),
         .allocator = allocator,
     };
-    @memcpy(w.ptr, syms.ptr, syms.len);
+    for (syms) |s, i| {
+        self.syms[i] = s;
+    }
+}
+
+pub fn create(allocator: Allocator, syms: []const u8) !Word {
+    var w: Word = undefined;
+    try init(&w, allocator, syms);
     return w;
 }
 
-pub fn appendSym(self: *const Self, sym: u8) !*Self {
-    const w = try self.allocator.create(Self);
-    errdefer self.allocator.destroy(w);
-    w.* = .{
-        .ptr = try self.allocator.alloc(u8, self.len + 1),
-        .len = self.len + 1,
-        .allocator = self.allocator,
+pub fn appendSym(self: *const Word, allocator: Allocator, sym: u8) !Word {
+    const w = Word{
+        .syms = try allocator.alloc(u8, self.syms.len + 1),
+        .allocator = allocator,
     };
-    @memcpy(w.ptr, self.ptr, self.len);
-    w.ptr[self.len] = sym;
+    for (self.syms) |s, i| {
+        w.syms[i] = s;
+    }
+    w.syms[self.syms.len] = sym;
     return w;
 }
 
-pub fn deinit(self: *Self) void {
-    self.allocator.free(self.ptr);
-    self.allocator.destroy(self);
+pub fn deinit(self: *Word) void {
+    self.allocator.free(self.syms);
+}
+
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
+
+test "Word.init" {
+    var w: Word = undefined;
+    try init(&w, std.testing.allocator, "abc");
+    defer w.deinit();
+    try expect(std.mem.eql(u8, "abc", w.syms));
+}
+
+test "Word.create" {
+    var w = try create(std.testing.allocator, "def");
+    defer w.deinit();
+    try expect(std.mem.eql(u8, "def", w.syms));
+}
+
+test "Word.appendSym" {
+    var w = try create(std.testing.allocator, "monke");
+    defer w.deinit();
+    var w2 = try w.appendSym(std.testing.allocator, 'y');
+    defer w2.deinit();
+    try expect(std.mem.eql(u8, "monkey", w2.syms));
 }
